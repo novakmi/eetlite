7da907168597781fb1178d5e6bb07a6c86bac9bb
Michal Novák <it.novakmi@gmail.com>
2026-09-15 22:28:56 +0200

    migrace na EET2.0 (libeetlite 2.0.0+) CI

--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 
 # eetlite 
 
+[![Run EET script](https://github.com/novakmi/eetlite-script/actions/workflows/ci.yml/badge.svg)](https://github.com/novakmi/eetlite-script/actions/workflows/ci.yml)
+[![Pipeline status](https://gitlab.com/novakmi/eetlite/badges/master/pipeline.svg)](https://gitlab.com/novakmi/eetlite/-/pipelines)
+
 Implementace "Elektronické evidence tržeb"  http://www.etrzby.cz  
 Implementation of  "Registration of sales" see  http://www.etrzby.cz
 
-Toto je ukázka implementace "Elektronické evidence tržeb" s použitím XML zpracování v Groovy 
-a s knihovnou `groovy-wslite`.  Implementace byla otestována oproti testovacímu prostředí EET.
+Toto je ukázka implementace "Elektronické evidence tržeb" s použitím XML zpracování v Groovy
+a knihovnou `libeetlite` 2.0.0. Implementace používá současné rozhraní EET 2.0/v4.
 
 Díky své jednoduchosti může být použitelná například pro prodej v kanceláři, provozovně a jiný sporadický prodej 
 (např. občasná úhrada přes platební bránu či kartou) za pomocí běžného PC.
 
-S použitím *zjednodušeného režimu* lze vytvořit účtenku i bez přítomnosti Internetu (např. u zákazníka) a tu pak
-odeslat dodatečně v ostrém režimu.
+EET 2.0 v této ukázce pracuje v online režimu a používá potvrzovací kód POK.
 
 Používání může vyžadovat určité znalosti příkazové řádky. Doporučuje se vše nejdříve vyzkoušet oproti 
 testovacímu prostředí EET (výchozí nastavení). 
@@ -39,6 +41,12 @@ testovacímu prostředí EET (výchozí nastavení).
 Viz soubor `LICENSE`
 
 ## Changelog
+
+* 2026-09-15 version `2.0.0`
+  * migrace na EET 2.0/v4 a `libeetlite` 2.0.0
+  * odstranění `groovy-wslite`, PKP, BKP a zjednodušeného režimu
+  * volitelná validace XML proti XSD
+  * heslo certifikátu lze zadat jako String nebo načíst ze souboru
 
 * 2020-03-03 version `0.7.0`
   * libeetlite ver 0.6.0
@@ -77,23 +85,21 @@ Viz soubor `LICENSE`
   * použití knihovny `libeetlite`    
   
 * 2016-10-13 version `0.0.3`
-  * upraveno pro testovací prostředí v3 (URL, namespace)    
+  * původní implementace pro testovací prostředí v3 (URL, namespace)
 
 * 2016-08-31 version `0.0.2`
   * přidána podpora volitelných parametrů
   * přidán popis parametrů pro editaci v souboru `./eetlite_run.groovy`  
 
-* 2016-08-16 version `0.0.1`  
+* 2016-08-16 version `0.0.1`
    * první verze
    * vytvoření a podepsání EET zprávy
    * odeslání EET zprávy na testovací prostředí
-   * zpracování odpovědi
-   * vytisknutí hodnoty 'FIK' z odpovědi
+   * zpracování odpovědi původního rozhraní
 
 ## Další vývoj (TODO)
 
 * kontrola správnosti parametrů 'tržby' a konfigurace (částečně implementováno)
-* validace XML zprávy oproti XML schématu
 * podpora pro hash (zakódování) hesla v konfiguračním souboru
 * zpracování chybové odpovědi
 * kontrola podpisu odpovědi
@@ -120,36 +126,37 @@ Aplikaci lze používat z příkazové řádky nebo spuštěním scriptu (2x kli
 * Nejprve se přesvědčte, že máte správně neinstalovanou podporu Groovy (příkaz `groovy --version`):
 
 `$ groovy --version`  
-`Groovy Version: 2.4.0 JVM: 1.8.0_101 Vendor: Oracle Corporation OS: Linux`
+`Groovy Version: 5.1.2 JVM: 25.0.1 Vendor: Eclipse Adoptium OS: Linux`
            
-* Upravte parametry tržby a konfigurace (cestu na soubor s klíčem, heslo a další volby) v souboru `eetlite_run.groovy`. 
+* Upravte parametry tržby a konfigurace (cestu k certifikátu, heslo a další volby) v souboru `eetlite_run.groovy`.
+  `cert_pass` lze vyplnit přímo jako String, nebo lze použít `cert_pass_file`.
+  XML validaci lze zapnout/vypnout pomocí `validate_xml`; cesta ke schématu je v `xsd_path`.
   Pro úpravu lze použít jakýkoliv editor, doporučuje se použít editor s podporou 
   syntaxe `groovy` a Linux formátu  (popř. `JEdit`, `notepad++`)
-* Spusťte aplikaci přikazem `groovy eetlite_run.groovy` (Windows, Linux, Mac) popřípadě 
-  příkazem `./eetlite_run.groovy` (Linux). Aplikaci je třeba spustit z adresáře, ve kterém jsou všechny její soubory.
+* Spusťte aplikaci z adresáře, ve kterém jsou všechny její soubory, příkazem
+  `groovy -cp /cesta/k/libeetlite/src/main/groovy eetlite_run.groovy`.
+  Pokud jste zdrojový adresář knihovny nakopírovali do skriptu, použijte `groovy -cp . eetlite_run.groovy`.
   Při úplně prvním spuštění je třeba vyčkat, než Groovy stáhne potřebné 'dependence' (většinou do adresáře 
   `.groovy` v domovském adresáři).
-* Po úspěšném zpracování se zobrazí jednoduchá účtenka dané tržby s hodnotou `FIK` a `BKP` a vytiskne do souboru 
+* Po úspěšném zpracování se zobrazí jednoduchá účtenka dané tržby s hodnotou `POK` a vytiskne do souboru
   (je-li povoleno):
    
 `$ groovy eetlite_run.groovy`
 
 ```
-eetlite 0.2.0 uctenka
+eetlite 2.0.0 uctenka
 https://sites.google.com/view/eetlite
 (https://github.com/novakmi/eetlite)
-Soubor: /eetlite_run_ostry_2017_01_21_11_06_42_eetlite.txt
+Soubor: /eetlite_run_online_2026_09_15_11_06_42_PC0_6460_ZQ42_eetlite.txt
 ====================================
 celk_trzba: 7896.00
-dat_trzby: 2017-01-21T18:45:15+01:00
-dic_popl: CZ00000019
+dat_trzby: 2026-09-15T18:45:15+02:00
+eic_popl: CZ00000019
 id_pokl: Q-126-R
-id_provoz: 123
+id_jednotky: 123
 porad_cis: 0/6460/ZQ42
-rezim: 0
-FIK: e91ca135-9f13-4eb1-8a2b-4bc759e33c9f-ff
-BKP: FAD6D023-E92678FE-410EE310-51580CA5-5697BDAA
-REZIM: bezny (s Internetem)
+POK: 58014b05-1bc5-46d0-8174-46bcf8ef8124-fa
+REZIM: EET 2.0 (online)
 CAS ZPRACOVANI: 988ms
 ```                      
 
@@ -168,10 +175,9 @@ popřípadě adresář `cert` (nebo změňte cestu jeho umístění ve skriptu).
 
 * Java  https://www.oracle.com/java/index.html
 * Groovy http://groovy-lang.org/
-* groovy-wslite  https://github.com/jwagenleitner/groovy-wslite
 * slf4j http://www.slf4j.org/
 * http://logback.qos.ch/
-* libeetlite - https://github.com/novakmi/libeetlite
+* libeetlite 2.0.0 - https://github.com/novakmi/libeetlite
 
 (_viz případné jednotlivé licence_)   
    
@@ -183,16 +189,43 @@ Změnou následujícího řádku v souboru `logback.groovy`:
     
 lze měnit úroveň logování na konzoli 
 
-V případě, že je třeba nutno provádět ladění (úpravu kódu) na úrovni knihovny `libeetlite`,
-lze doporučit následující postup:
+Skript nepoužívá `@Grab` pro `libeetlite`, protože artefakt nemusí být dostupný v repozitáři
+Grape. K provozu je proto nutné zpřístupnit zdrojové soubory knihovny na classpath:
 
-* odstranit `@Grab` řádek  `@Grab("com.github.novakmi:libeetlite:0.1.0"),`, který imporutje knihovnu
-* provést `clone` knihovny `libeetlite` pomocí `git clone https://github.com/novakmi/libeetlite`
-    * spustit s `classpath` ukazující na zdrojové soubory `libeetlite` příkazem `groovy -cp <adresář s libeetlite>/libeetlite/src/main/groovy/ eetlite_run.groovy`
-    * (nebo) nakopírovat obsah adresáře  `<adresář s libeetlite>/libeetlite/src/main/groovy` do adresáře se souborem `eetlite_run.groovy`
+* naklonujte nebo stáhněte knihovnu `libeetlite`,
+* spusťte skript s classpath na její zdrojové soubory:
 
-* nyní lze provádět změny ve zdrojových souborech `libeetlite`, které se projeví v ihned dalším spuštění 
-  (v `groovy` není třeba nic překládat)
+`groovy -cp /cesta/k/libeetlite/src/main/groovy eetlite_run.groovy`
+
+Alternativně nakopírujte adresář `libeetlite/src/main/groovy/com` do adresáře skriptu
+a spusťte `groovy -cp . eetlite_run.groovy`.
+
+Skript si přes `@Grab` stáhne pouze podpůrné knihovny `logback` a `xmlsec`. Při použití
+zdrojů `libeetlite` není nutné knihovnu překládat.
+
+## CI
+
+GitHub Actions i GitLab CI spouštějí skript při každém pushi a jednou týdně podle
+plánované pipeline. GitHub Actions používá matici runnerů
+`ubuntu-latest`, `windows-latest` a `macos-latest`, takže je skript ověřen na
+všech třech platformách. CI nejprve zpřístupní zdrojový adresář knihovny
+`libeetlite` a potom spustí:
+
+`groovy -cp libeetlite/src/main/groovy eetlite_run.groovy`
+
+Certifikát se načítá z `cert/CA_EET-Playground-CZ00000019.p12`, heslo z
+`cert/password_pokladni_cert_playground.txt` a validace XML z lokálního souboru
+`EETXMLSchema.xsd`. CI tedy nepotřebuje další aplikační konfiguraci ani externí
+uložení certifikátu.
+
+GitHub Actions používá
+https://github.com/marketplace/actions/setup-groovy[`wtfjoke/setup-groovy`]
+s přesně určenou verzí Groovy `5.1.1` a Java `21`. Není proto potřeba explicitní
+`docker run` ani instalace Groovy přes systémový balíček. GitLab CI používá
+oficiální Docker image z
+https://github.com/groovy/docker-groovy[`groovy:5.1.1-jdk21`], takže verze Groovy
+je v CI pevně určena. Tag `groovy:5.1.2-jdk21` momentálně není dostupný;
+aktualizaci verze je vhodné provést současně s ověřením dostupného tagu.
 
 ## Kontakt
 
--- eetlite_run.groovy
+++ eetlite_run.groovy
@@ -1,187 +1,158 @@
 #!/usr/bin/env groovy
+
 import com.github.novakmi.libeetlite.EetUtil
 import com.github.novakmi.libeetlite.EetXml
-@Grapes([
-        @GrabConfig(systemClassLoader = true), //logback config can be read, thanks to https://gist.github.com/grimrose/3759266
-        @Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.2.3'),
-        @Grab('com.github.groovy-wslite:groovy-wslite:1.1.3'),
-        @Grab("com.github.novakmi:libeetlite:0.7.0"),
-])
-
 import groovy.util.logging.Slf4j
 
-/* (c) 2016 - 2020 Michal Novák, it.novakmi@gmail.com, see LICENSE file */
-import wslite.soap.SOAPClient
-import wslite.soap.SOAPResponse
+@Grapes([
+        @GrabConfig(systemClassLoader = true),
 
+        @Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.4.14'),
+        @Grab(group = 'org.apache.santuario', module = 'xmlsec', version = '4.0.4')
+
+        // Java 8: comment out the versions above and uncomment these:
+//         @Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.3.14'),
+//         @Grab(group = 'org.apache.santuario', module = 'xmlsec', version = '3.0.3')
+])
+
+/* (c) 2016 - 2026 Michal Novák, it.novakmi@gmail.com, see LICENSE file */
 @Slf4j
-class EetRunner { // class is used for Slf4j annotation
-    def version = "0.7.0"
+class EetRunner {
+    def version = "2.0.0"
     def scriptName = getClass().protectionDomain.codeSource.location.path
+    def printToFile = 1
 
     // ****** UPRAVIT PARAMETRY *****
-    //viz http://www.etrzby.cz/cs/technicka-specifikace
-    // ------ VE VETSINE PRIPADU STACI UPRAVIT POUZE TUTO CAST -----
-    def printToFile = 1  //0 .. uctenka jen na obrazovku, 1 .. uctenka i do souboru <jmeno>_rezim_<datum>_PC<porad_cis>_eetlite.txt
     def trzba_var = [
-            porad_cis : "0/6460/ZQ42",               // poradove cislo uctenky (1-20 znaku)
-            dat_trzby : EetUtil.nowToIso(),         //"2017-03-05T18:45:15+01:00", // datum a cas prijeti trzby dle ISO 8601, rrrr-mm-ddThh:mm:ss±hh:mm (±hh je +01 pro zimni cas, +02 pro letni cas)
-            //  EetUtil.nowToIso() ... pro aktualni cas dle ISO 8601
-            celk_trzba: "7897.00",                   // celkova castka trzby
-            /* nepovinne polozky (odstranit komentar //)*/
-//            zakl_nepodl_dph : "0.00",                 // celkova castka plneni osvobozenych od DPH, ostatnich plneni
-//            zakl_dan1 : "0.00",                       // celkovy zaklad dane se zakladni sazbou DPH
-//            dan1: "0.00",                             // celkova DPH se zakladni sazbou
-//            zakl_dan2 : "0.00",                       // celkovy zaklad dane s prvni snizenou sazbou DPH
-//            dan2: "0.00",                             // celkova DPH s prvni snizenou sazbou
-//            zakl_dan3 : "0.00",                       // celkovy zaklad dane s druhou snizenou sazbou DPH
-//            dan3: "0.00",                             // celkova DPH s druhou snizenou sazbou
-//            cest_sluz : "0.00",                       // celková castka v rezimu DPH pro cestovni sluzbu
-//            pouzit_zboz1 : "0.00",                    // celková castka v rezimu DPH pro prodej pouziteho zbozi se zakladni sazbou
-//            pouzit_zboz2 : "0.00",                    // celková castka v rezimu DPH pro prodej pouziteho zbozi s prvni snizenou sazbou
-//            pouzit_zboz3 : "0.00",                    // celková castka v rezimu DPH pro prodej pouziteho zbozi s druhou snizenou sazbou
-//            urceno_cerp_zuct: "0.00",                  // celková castka plateb urcena k naslednemu cerpani nebo zuctovani
-//            cerp_zuct: "0.00",                        // celková castka plateb, ktere jsou naslednym cerpanim nebo zuctovanim platby
+            porad_cis : "0/6460/ZQ42",
+            dat_trzby : EetUtil.nowToIso(),
+            celk_trzba: "7897.00",
+            // urceno_cerp_zuct: "0.00",
+            // cerp_zuct: "0.00",
     ]
-    // ------
 
-    // Nasledujici casti se upravi jednou, v ostatnich pripadech jsou jiz vetsinou stejne
     def trzba_fix = [
-            rezim    : "0",                      // 0 .. bezny rezim (s Internetem), 1 .. zjednoduseny rezim (bez Internetu)
-            dic_popl : "CZ00000019",             // DIC poplatnika, ktery odesila datovou zpravu (mel by se schodovat s certifikatem nize)
-            id_provoz: "123",                    // oznaceni provozovny (1. az 5. cif. cislo)
-            id_pokl  : "Q-126-R",                // oznaceni pokladniho zarizeni (1-20 znaku)
-            /* nepovinne polozky (odstranit //)*/
-//           dic_poverujiciho : "CZ1212121218",  // DIC poverujiciho poplatnika (nepovinne)
+            eic_popl    : "CZ00000019",
+            id_jednotky: "123",
+            id_pokl     : "Q-126-R",
+            // eic_poverujiciho: "CZ1212121218",
+            // povereni_vice_popl: "true",
     ]
 
     def hlavicka = [
-            overeni      : "0",   //0 .. ostry mod, 1 ... overovaci mod
-            prvni_zaslani: "1",   //1 .. prvni zaslani trzby, 0 .. opakovane zaslani trzby
+            overeni      : "false",
+            prvni_zaslani: "true",
     ]
 
     def config_fix = [
-            cert_popl: new FileInputStream("cert/EET_CA1_Playground-CZ00000019.p12"),  // stream na certificat poplatnika (cesta FileInputStream je relativni k adresari eetlite)
-            cert_pass: "eet",                             // heslo cetrifikatu (zatim text, pozdeji bude zasifrovano)
-            url      : "https://pg.eet.cz:443/eet/services/EETServiceSOAP/v3", // url EET (testovaci prostredi)
-            //url: "https://prod.eet.cz:443/eet/services/EETServiceSOAP/v3", // url EET (produkcni prostredi)
+            cert_popl_path: "cert/CA_EET-Playground-CZ00000019.p12",
+            cert_pass     : null, // alternativně zadejte heslo přímo jako String
+            cert_pass_file: "cert/password_pokladni_cert_playground.txt",
+            url           : "https://pg.trzbyeet.gov.cz/eet/services/EETServiceSOAP/v4",
+            validate_xml  : true,
+            xsd_path      : "EETXMLSchema.xsd",
     ]
-    // ****** KONEC PRO UPRAVU PARAMETRU *****
+    // ****** KONEC PARAMETRU *****
 
     def config = hlavicka + trzba_var + trzba_fix + config_fix
 
-    def getReceipt(message, fileName, fik, rezim, duration) {
-        log.debug "==> getReceipt"
-        def nl = System.getProperty("line.separator");
+    def getReceipt(message, fileName, pok, duration) {
+        def nl = System.getProperty("line.separator")
         def ret = "eetlite ${version} uctenka" + nl
         ret += "https://sites.google.com/view/eetlite" + nl
         ret += "(https://github.com/novakmi/eetlite)" + nl
         if (printToFile) {
-            def file = new File(fileName).getPath()
-            ret += "Soubor: ${file}" + nl
+            ret += "Soubor: ${new File(fileName).path}" + nl
         }
         ret += "====================================" + nl
-        for (i in EetXml.dataFields.keySet()) {
-            if (config[i]) {
-                ret += "${i}: ${config[i]}" + nl
+        EetXml.dataFields.keySet().each { key ->
+            if (config[key] != null) {
+                ret += "${key}: ${config[key]}" + nl
             }
         }
-
-        ret += "BKP: ${message.bkp}" + nl
-        if (!rezim) {
-            ret += "FIK: ${fik}" + nl
-        } else {
-            ret += "PKP: ${message.pkp}" + nl
-        }
-        ret += "REZIM: ${!rezim ? "bezny (s Internetem)" : "zjednoduseny (bez Internetu)"}" + nl
+        ret += "POK: ${pok}" + nl
+        ret += "REZIM: EET 2.0 (online)" + nl
         ret += "CAS ZPRACOVANI: ${duration}ms"
-        log.debug "<== getReceipt ret=${ret}"
         return ret
     }
 
     def fileWithSubStringExists(dirName, fileSubString) {
-        log.debug "==> fileWithSubStringExists"
-        def retVal
-        def dir = new File(dirName)
-        def filter = new FilenameFilter() {
-            boolean accept(File path, String filename) {
-                return filename.contains(fileSubString as String)
+        def files = new File(dirName).listFiles { file ->
+            file.name.contains(fileSubString as String)
+        }
+        return files != null && files.length > 0
+    }
+
+    def validateXmlIfEnabled(config, xml) {
+        if (!config.validate_xml) {
+            log.info "Validace XML proti XSD je vypnuta"
+            return
+        }
+        def xsd = new File(config.xsd_path)
+        if (!xsd.isFile()) {
+            throw new FileNotFoundException("XSD soubor nebyl nalezen: ${xsd.absolutePath}")
+        }
+        xsd.withInputStream { stream ->
+            def errors = EetUtil.validateXml(xml, stream)
+            if (!errors.empty) {
+                throw new IllegalStateException("XML neprošlo validací: ${errors.join('; ')}")
             }
         }
-        def files = dir.listFiles(filter)
-        retVal = files.size() > 0
-        log.debug "<== fileWithSubStringExists retVal={}", retVal
-        return retVal
     }
 
     def processEet(config, message, fileName) {
-        log.info "==> processEet"
-        def timeIn = new Date()
-        def rezim = EetUtil.isZjednodusenyRezim(config)
-        config.cert_popl.close() // close file input stream
-
+        def timeIn = System.currentTimeMillis()
         def toSend = message.xml.toString()
-        log.debug "bkp: {}", message.bkp
-        log.debug "toSend: {}", toSend
-        def fik = ""
-        if (!rezim) { // ostry rezim - Internet
-            SOAPClient client = new SOAPClient(config.url)
-            SOAPResponse response = client.send(toSend)
+        validateXmlIfEnabled(config, toSend)
 
-            log.trace "response {}", response
-            def respText = response.text
-            log.debug "indented response: {}", EetXml.indentXml(respText)
-
-            //TODO processing error messages
-            //TODO verify signed response
-            def processed = EetXml.processResponse(respText)
-            fik = processed.fik
-        } else {
-            log.debug("Message not send, 'zjednoduseny rezim' ${config.rezim}")
+        def responseText = EetUtil.sendSOAPRequest(config.url, toSend)
+        log.debug "indented response: {}", EetXml.indentXml(responseText)
+        def processed = EetXml.processResponse(responseText)
+        if (processed.failed) {
+            throw new IllegalStateException("EET odpověď obsahuje chybu: ${processed.errors}")
         }
-        def duration = new Date(new Date().getTime() - timeIn.getTime()).getTime()
-        def receipt = getReceipt(message, fileName, fik, rezim, duration)
+
+        def duration = System.currentTimeMillis() - timeIn
+        def receipt = getReceipt(message, fileName, processed.pok, duration)
         println receipt
         if (printToFile) {
-            new File(fileName).write(receipt)
+            new File(fileName).write(receipt, "UTF-8")
         }
-        log.debug "fik {}", fik
-        log.info "<== processEet"
     }
 
     def isValid(config, message, fileSubString) {
-        log.info "==> isValid"
-        def retVal = true
-        //TODO validate message against schema
-        // TODO validate config (mandatory prams, regex patterns)
-
-        if (retVal && printToFile) {
-            if (fileWithSubStringExists(".", fileSubString)) {
-                println "========================================================================="
-                println "Uctenka s cislem ${config.porad_cis} jiz existuje, opravte pořadové číslo"
-                println "nebo přesuňte/odstraňte soubor obsahující ${fileSubString}"
-                println "========================================================================="
-                retVal = false
-            }
+        if (message.failed) {
+            println "Zprávu se nepodařilo vytvořit: ${message.errors}"
+            return false
         }
-
-        log.info "<== isValid retVal={}", retVal
-        return retVal
+        if (printToFile && fileWithSubStringExists(".", fileSubString)) {
+            println "Uctenka s cislem ${config.porad_cis} jiz existuje."
+            return false
+        }
+        return true
     }
 
     def run() {
-        log.info "==> run"
         log.info "eetlite ver {}", version
         println "eetlite script Groovy: ${GroovySystem.version} JVM: ${System.getProperty("java.specification.version")}"
-        def orderInFileName = "PC${config.porad_cis.replaceAll(File.separator, "_")}"
-        def rezim = config.rezim != "0"
+
+        config.cert_pass = config.cert_pass ?: new File(config.cert_pass_file).text.trim()
+        if (!config.cert_pass) {
+            throw new IllegalArgumentException("Nebylo zadano heslo certifikatu.")
+        }
+
+        def orderInFileName = "PC${config.porad_cis.replaceAll(/[\\\/:]/, "_")}"
         def fileName = scriptName.replace(".groovy",
-                "_${!rezim ? "ostry" : "zjednoduseny"}_${new Date().format("yyyy_MM_dd_hh_mm_ss")}_${orderInFileName}_eetlite.txt")
-        def message = EetXml.makeMsg(config)
-        if (isValid(config, message, orderInFileName)) {
-            processEet(config, message, fileName)
-        } else {
-            println "Parametry neprošly validací, EET tržba se neposlala!"
+                "_online_${new Date().format("yyyy_MM_dd_HH_mm_ss")}_${orderInFileName}_eetlite.txt")
+
+        config.cert_popl = new FileInputStream(config.cert_popl_path)
+        try {
+            def message = EetXml.makeMsg(config)
+            if (isValid(config, message, orderInFileName)) {
+                processEet(config, message, fileName)
+            }
+        } finally {
+            config.cert_popl.close()
         }
     }
 }
